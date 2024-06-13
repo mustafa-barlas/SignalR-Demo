0f7b0943f25c844a92ed9208536eaefc7ee9ca23
Mustafa <mustafa.barlas1146@gmail.com>
2024-06-13 12:50:06 +0300

    files added

---
+++ SignalRServerExamle/Hubs/MyHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServerExamle.Hubs;
+
+public class MyHub : Hub
+{
+    public async Task SendMessageAsync(string message)
+    {
+      await  Clients.All.SendAsync("receiveMessage", message);
+    }
+}
---
+++ SignalRServerExamle/Program.cs
@@ -0,0 +1,39 @@
+using SignalRServerExamle.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR(); // ***************************************************
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+{
+    policy.AllowAnyMethod().AllowCredentials().AllowAnyHeader().SetIsOriginAllowed(origin => true);
+})); // **************************************************
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseCors();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.UseEndpoints(endpoints =>
+{
+    // endpoints.MapDefaultControllerRoute();
+    endpoints.MapHub<MyHub>("/myhub");
+});
+
+app.Run();
